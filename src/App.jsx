2759049236ac7621fe88c3b9ab261a19bebc00ba
sstech02 <samuelsch002@gmail.com>
2026-03-11 23:38:33 -0500

    chore: sync main with gh-pages deploy changes

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 function App () {
   return (
-    <Router>
+    <Router basename={process.env.PUBLIC_URL || '/'}>
       <div className='App'>
         <Routes>
           <Route path='/' element={<Home />} />
